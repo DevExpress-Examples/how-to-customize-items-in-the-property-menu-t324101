2890c6946afee9ad1f246c02cd198c393a023d0f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.ComponentModel;
+
+namespace DXSample {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            DataContext = new Contact {
+                FirstName = "Carolyn",
+                LastName = "Baker",
+                Email = "carolyn.baker@example.com",
+                Phone = "(555)349-3010",
+                Address = "1198 Theresa Cir",
+                City = "Whitinsville",
+                State = "MA",
+                Zip = "01582",
+            };
+            InitializeComponent();
+        }
+    }
+    public class Contact {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CreditCardNumber { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public byte[] Photo { get; set; }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Customize Items in the Property Menu
+
+
+Property Menu items can be customized by adding <a href="https://documentation.devexpress.com/#WPF/CustomDocument6646">bar items</a> and <a href="https://documentation.devexpress.com/#WPF/CustomDocument7045">actions</a> to the <a href="https://documentation.devexpress.com/#WPF/CustomDocument15720">PropertyDefinition.MenuCustomizations</a> collection as described at <a href="https://documentation.devexpress.com/#WPF/CustomDocument15720">Customizing Property Menus</a>.
+
+<br/>
+
+
